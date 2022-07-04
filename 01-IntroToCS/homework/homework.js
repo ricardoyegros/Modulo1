b4970df0fe3d08663924f11667973998ce5567c9
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-07-04 19:09:35 -0300

    homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,30 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let result = [];
+  let arrayOfNum = [...num + ""];
+  let arrayInReverse = arrayOfNum.reverse();
+  for (let i = 0; i < arrayInReverse.length; i++) {
+    let step = 2 ** i * arrayInReverse[i];
+    result.push(step);
+  }
+  return result.reduce((a,b) => a + b);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
-}
-
+  let n = [];
+  let a = num;
+  while (Math.floor(a / 2) > 0) {
+    let middleResult = (a % 2);
+    n.unshift(middleResult);
+    a = Math.floor(a / 2);
+    if(Math.floor(a / 2) === 0) {
+        n.unshift(1);
+    }
+  }
+  return n.join("");
+  }
 
 module.exports = {
   BinarioADecimal,
